464b8f87740d957c9962bfc335057529cd128773
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2018-02-06 01:26:13 -0500

    Make Prettier
    https://prettier.io/playground/
    
    OPTIONS
    
    --parser "babylon"
    --print-width 80
    --tab-width 4
     --use-tabs false
    
     --no-semi false
     --single-quote false
     --no-bracket-spacing false
     --jsx-bracket-same-line false
    --arrow-parens  avoid
    --trailing-comma all
    Markdown
    --prose-wrap preserve
    Special
     --insert-pragma false
     --require-pragma false
    Debug
     show AST false
     show doc false
     show second format false

--- better-watchseries.user.js
+++ better-watchseries.user.js
@@ -6,13 +6,12 @@
 // @license      MIT
 //
 // @namespace    https://github.com/andrewjmetzger/
-// @supportURL   https://github.com/andrewjmetzger/better-watchseries
 // @updateURL    https://openuserjs.org/meta/andrewjmetzger/Better_WatchSeries.meta.js
-// @version      1.10.4
+// @version      1.10.5
 //
 // @grant        unsafeWindow
 // @run-at       document-end
-// 
+//
 // @match        *://*.dwatchseries.ac/*
 // @match        *://*.daclips.in/*
 // @match        *://*.gorillavid.in/*
@@ -21,94 +20,98 @@
 // @match        *://*.streamplay.top/*
 // ==/UserScript==
 
-
 /*************************************************
-*     SETTINGS START HERE - CHANGE SOME STUFF    *
-*  Warning: Leave the semicolons alone, or else  *
-**************************************************/
+ *     SETTINGS START HERE - CHANGE SOME STUFF    *
+ *  Warning: Leave the semicolons alone, or else  *
+ **************************************************/
 
+// WS_DOMAIN : The current WatchSeries domain. For example, `watchseries.to`.
+//             (Case-insensitive)
 
-// WS_DOMAIN : The current WatchSeries domain. For example, `watchseries.to`. (Case-insensitive)
+WS_DOMAIN = "www.dwatchseries.ac";
 
-WS_DOMAIN = 'www.dwatchseries.ac';
-
-
-// WS_PATH : In the full URL of a video on WatchSeries, the part after the domain, and before the video host.
-//           Usually just one word. For example, in `http://www.dwatchseries.ac/link/some-video-host.not/12345678`, 
+// WS_PATH : In the full URL of a video on WatchSeries, the part after the
+//           domain, and before the video host. Usually just one word.
+//           For example, in `http://watchseries.ac/link/some-host.not/12345678`,
 //           WS_PATH is `link`. (Case-insensitive)
 
-WS_PATH = 'link';
+WS_PATH = "link";
 
+// WS_TARGET : The CSS class for the WatchSeries footer. Leave it alone unless
+//             you undersand exactly what this does. (Case-sensitive)
 
-// WS_TARGET : The CSS class for the footer. Don't change it if you don't understand. (Case-sensitive)
-
-WS_TARGET = ' video-footer';
-
+WS_TARGET = " video-footer";
 
 /*************************************************
-*      END OF SETTINGS - STOP CHANGING STUFF     *
-**************************************************/
+ *      END OF SETTINGS - STOP CHANGING STUFF     *
+ **************************************************/
 
-
-console.log('Better WatchSeries has loaded. Hello there.');
-console.log('Version: ' + GM_info.script.version);
+console.log("Better WatchSeries has loaded. Hello there.");
+console.log("Version: " + GM_info.script.version);
 
 var url = location.href;
-console.log('Current URL: ' + url);
+console.log("Current URL: " + url);
 
-var WS_URL = WS_DOMAIN + '/' + WS_PATH;
-
+var WS_URL = WS_DOMAIN + "/" + WS_PATH;
 
 if (url.indexOf(WS_URL) != -1) {
-    console.log('Site found: WatchSeries');
-    dest =  document.getElementsByClassName(WS_TARGET)[0].children[2].href;
+    console.log("Site found: WatchSeries");
+    dest = document.getElementsByClassName(WS_TARGET)[0].children[2].href;
     dest = dest.toString();
-    console.log('dest == ' + dest);
-    console.log('Redirecting to : ' + dest);
+    console.log("dest == " + dest);
+    console.log("Redirecting to : " + dest);
     location.href = dest;
 }
 
 function checkHosts(hosts) {
-    for (i in hosts) if (window.location.hostname == hosts[i] || window.location.hostname == 'www.' + hosts[i]) {
-	return true;
-    }
+    for (i in hosts)
+        if (
+            window.location.hostname == hosts[i] ||
+            window.location.hostname == "www." + hosts[i]
+        ) {
+            return true;
+        }
     return false;
 }
 
 function clickButton(button, hosts) {
     button.disabled = false;
-    
     button.click();
 }
 
 function clickButtonByIdOnHosts(buttonId, hosts) {
-    if (checkHosts(hosts)) try {
-	var button = document.getElementById(buttonId);
-	clickButton(button);
-    } catch (err) {}
+    if (checkHosts(hosts))
+        try {
+            var button = document.getElementById(buttonId);
+            clickButton(button);
+        } catch (err) {}
 }
 
 function clickButtonByNameOnHosts(buttonName, hosts) {
-    if (checkHosts(hosts)) try {
-	var button = document.getElementsByName(buttonName)[0];
-	clickButton(button);
-    } catch (err) {}
+    if (checkHosts(hosts))
+        try {
+            var button = document.getElementsByName(buttonName)[0];
+            clickButton(button);
+        } catch (err) {}
 }
 
 function clickButtonByClassOnHosts(buttonClass, hosts) {
-    if (checkHosts(hosts)) try {
-        var button = document.querySelector(buttonClass);
-        clickButton(button);
-    } catch (err) {}
+    if (checkHosts(hosts))
+        try {
+            var button = document.querySelector(buttonClass);
+            clickButton(button);
+        } catch (err) {}
 }
 
 try {
-    var hosts = ['gorillavid.in', 'daclips.in', 'movpod.in', 'streamplay.top'];
-    clickButtonByIdOnHosts('btn_download', hosts);
+    var hosts = ["gorillavid.in", "daclips.in", "movpod.in", "streamplay.top"];
+    clickButtonByIdOnHosts("btn_download", hosts);
 
-    var hosts = ['thevideo.cc'];
-    clickButtonByClassOnHosts('button.btn.btn-lg.btn-primary.bottom-buffer', hosts);
-    
-    } catch(err) {
-    console.log('Sorry, could not click the button.');
-    }
+    var hosts = ["thevideo.cc"];
+    clickButtonByClassOnHosts(
+        "button.btn.btn-lg.btn-primary.bottom-buffer",
+        hosts,
+    );
+} catch (err) {
+    console.log("Error: Better WatchSeries could not click the button.");
+}
