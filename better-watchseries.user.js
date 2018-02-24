c1b46e27f2302b3da9f36636ab9b4c0dbe192491
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2018-02-24 01:41:13 -0500

    Update better-watchseries.user.js

--- better-watchseries.user.js
+++ better-watchseries.user.js
@@ -7,7 +7,7 @@
 //
 // @namespace    https://github.com/andrewjmetzger/
 // @updateURL    https://openuserjs.org/meta/andrewjmetzger/Better_WatchSeries.meta.js
-// @version      2.1.2
+// @version      2.1.3
 //
 // @grant        unsafeWindow
 // @run-at       document-end
