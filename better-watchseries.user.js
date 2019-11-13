c1613ae0d409eabed6ffe95f572b67ffaf62c661
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2019-11-13 13:25:59 -0500

    Update support for seriesfree.to

--- better-watchseries.user.js
+++ better-watchseries.user.js
@@ -78,8 +78,7 @@ if (url.indexOf("/freecale.html") != -1) {
 } else if (url.indexOf("/open/cale") != -1) {
   console.log("Site found: open/cale-like");
   dest = document.querySelector(
-    "#app > section > div.view.cf > div > div \
-                                  > article:nth-child(8) > div.actions.grid-1.grid-lg-8-24 > a"
+    "#app > section > div.view.cf > div > div > article:nth-child(8) > div.actions.grid-1.grid-lg-8-24 > a"
   ).href;
   dest = dest.toString();
   console.log("dest == " + dest);
